539c39782dd7b45a3b7350c0b6e2f18a65d3f49b
derrick b <dbozich1@gmail.com>
2018-08-13 08:19:44 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,4 +18,15 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
+
+	def edit
+		@post = Post.find_by_id(params[:id])
+	end
+
+	def update
+		@post = Post.find_by_id(params[:id])
+		@post.title = params[:title]
+		@post.description = params[:description]
+
+	end
 end
